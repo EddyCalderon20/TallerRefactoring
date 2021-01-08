b6e56832fda983000591c6f1c247e5a870eb9afd
EddyCalderon20 <49259893+EddyCalderon20@users.noreply.github.com>
2021-01-07 23:39:52 -0500

    cambios en la clase Materia

--- src/modelos/Materia.java
+++ src/modelos/Materia.java
@@ -4,8 +4,15 @@ public class Materia {
     public String codigo;
     public String nombre;
     public String facultad;
-    public double notaInicial;
-    public double notaFinal;
-    public double notaTotal;
-    
+    private double notaInicial;
+    private double notaFinal;
+    private double notaTotal;
+    public double getNotaInicial() {
+        return notaInicial;
+    }
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+    public double getNotaTotal() { return notaTotal;
+    }
 }
